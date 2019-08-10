b0f6d61c9eca6540f0be8bec93c192f7945015e0
Alejandro <aesbetancourt@outlook.com>
2019-08-10 18:54:54 -0400

    Improved Welcome, Login and Register template
    Added a /assets inside /public for all assets of this templates (css, fontawesome, images).

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,77 +1,56 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
-
-                <div class="card-body">
-                    <form method="POST" action="{{ route('register') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
-
-                                @error('name')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Register') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
+      <div>
+        <div class="register-photo">
+          <div class="form-container">
+            <div class="image-holder"></div>
+            <form method="POST" action="{{ route('register') }}">
+              @csrf
+              <h2 class="text-center"><strong>Crea</strong>&nbsp;una cuenta.</h2>
+              <div class="form-group">
+                <input id="name" placeholder="Nombre" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+                @error('name')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                @enderror
+              </div>
+              <div class="form-group">
+                <input class="form-control" type="text" name="cedula" placeholder="Cédula" >
+               </div>
+              <div class="form-group">
+                <input class="form-control" type="text" name="telefono" placeholder="Telefono" >
+              </div>
+              <div class="form-group">
+                <input  id="email" placeholder="Email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
+                @error('email')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                @enderror
+              </div>
+              <div class="form-group">
+                <input id="password" placeholder="Contraseña" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+                @error('password')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                @enderror
+              </div>
+              <div class="form-group">
+                <input id="password-confirm" placeholder="Confirmar Contraseña" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+              </div>
+              <div class="form-group">
+                <button class="btn btn-primary btn-block" type="submit" style="background-color: rgb(37,55,102);">
+                    Registrate!
+                </button>
+              </div>
+                <a class="already" href="/login">
+                    Ya tienes una cuenta? Inicia aqui.
+                </a>
+            </form>
+          </div>
         </div>
-    </div>
-</div>
+      </div>
 @endsection
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,92 +4,113 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Laravel</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
+        <link rel="stylesheet" href="/css/app.css">
+        <link rel="stylesheet" href="/assets/css/Header-Blue.css">
+        <link rel="stylesheet" href="/assets/css/Highlight-Clean.css">
+        <link rel="stylesheet" href="/assets/css/Footer-Dark.css">
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-                        <a href="{{ route('register') }}">Register</a>
-                    @endauth
-                </div>
-            @endif
+        <div>
+          <div class="header-blue">
+            <nav class="navbar navbar-dark navbar-expand-md navigation-clean-search">
+              <div class="container"><a class="navbar-brand" href="#">FreeBook.com</a><button data-toggle="collapse" class="navbar-toggler" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse"
+                     id="navcol-1">
+                  <ul class="nav navbar-nav">
+                    <li class="nav-item" role="presentation">
 
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
+                    </li>
+                  </ul>
+                  <form class="form-inline mr-auto" target="_self">
+                    <div class="form-group">
+                      <label for="search-field">
+                        <i class="fa fa-search"></i>
+                      </label>
+                      <input class="form-control search-field" type="search" id="search-field" name="search">
+                    </div>
+                  </form>
+                  @if (Route::has('login'))
+                         @auth
+                            <span class="nav-item navbar-text" role="presentation">
+                                <a class="btn btn-light action-button" role="button" href="{{ url('/home') }}">
+                                    Accede
+                                </a>
+                            </span>
+                         @else
+                              <span class="nav-item navbar-text" role="presentation">
+                                  <a class="login" href="{{ route('login')}}">
+                                    Iniciar Sesión
+                                  </a>
+                              </span>
+                              <span class="nav-item navbar-text" role="presentation">
+                                  <a class="btn btn-light action-button" role="button" href="{{ route('register') }}">
+                                    Regístrate
+                                  </a>
+                              </span>
+                        @endauth
+                  @endif
                 </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+              </div>
+            </nav>
+            <div class="container hero">
+              <div class="row">
+                <div class="col-12 col-lg-6 col-xl-5 offset-xl-1">
+                  <h1>Libros a donde quiera que vayas.</h1>
+                  <p>Mauris egestas tellus non ex condimentum, ac ullamcorper sapien dictum. Nam consequat neque quis sapien viverra convallis. In non tempus lorem. </p><button class="btn btn-light btn-lg action-button" type="button">Leer Mas</button></div>
+                <div
+                  class="col-md-5 col-lg-5 offset-lg-1 offset-xl-0 d-none d-lg-block phone-holder">
+                  <div class="iphone-mockup"><img class="device" src="../assets/img/iphone.svg">
+                    <div class="screen"></div>
+                  </div>
                 </div>
+              </div>
             </div>
+          </div>
+        </div>
+        <div class="highlight-clean">
+          <div class="container">
+            <div class="intro">
+              <h2 class="text-center">Libros, Libros...</h2>
+              <p class="text-center">Nunc luctus in metus eget fringilla. Aliquam sed justo ligula. Vestibulum nibh erat, pellentesque ut laoreet vitae. </p>
+            </div>
+            <div class="buttons">
+              <a class="btn btn-primary" role="button" href="#">
+                Lorem.
+              </a>
+              <button class="btn btn-light" type="button">
+                Lorem.
+              </button>
+            </div>
+          </div>
+        </div>
+        <div class="footer-dark">
+          <footer>
+            <div class="container">
+              <div class="row">
+                <div class="col-sm-6 col-md-3 item">
+                  <h3>Servicios</h3>
+                  <ul>
+                    <li><a href="#">Lorem ipsum.</a></li>
+                    <li><a href="#">Lorem ipsum.</a></li>
+                    <li><a href="#">Lorem ipsum.</a></li>
+                  </ul>
+                </div>
+                <div class="col-sm-6 col-md-3 item">
+                  <h3>Acerca</h3>
+                  <ul>
+                    <li><a href="#">Lorem.</a></li>
+                    <li><a href="#">Lorem.</a></li>
+                    <li><a href="#">Lorem.</a></li>
+                  </ul>
+                </div>
+                <div class="col-md-6 item text">
+                  <h3>FreeBook.com</h3>
+                  <p>Praesent sed lobortis mi. Suspendisse vel placerat ligula. Vivamus ac sem lacus. Ut vehicula rhoncus elementum. Etiam quis tristique lectus. Aliquam in arcu eget velit pulvinar dictum vel in justo.</p>
+                </div>
+              </div>
+              <p class="copyright">FreeBook.com © 2019</p>
+            </div>
+          </footer>
         </div>
     </body>
 </html>
