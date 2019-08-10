e02d82e0481e73664593ab9bc3e49b4a93ed9a15
Alejandro <aesbetancourt@outlook.com>
2019-08-10 00:20:18 -0400

    Added a new logo and user img

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -35,8 +35,8 @@ scratch. This page gets rid of all links and provides the needed markup only.
   <!-- Main Sidebar Container -->
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="index3.html" class="brand-link">
-      <img src="./img/logo.png" alt="" class="brand-image img-circle elevation-3"
+    <a href="#" class="brand-link">
+      <img src="./img/logo.png" alt="" class="brand-image img-box elevation-3"
            style="opacity: .8">
       <span class="brand-text font-weight-light">FreeBook.com</span>
     </a>
@@ -46,7 +46,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
       <!-- Sidebar user panel (optional) -->
       <div class="user-panel mt-3 pb-3 mb-3 d-flex">
         <div class="image">
-          <img src="dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+          <img src="./img/user.png" class="img-circle elevation-2">
         </div>
         <div class="info">
           <a href="#" class="d-block">
