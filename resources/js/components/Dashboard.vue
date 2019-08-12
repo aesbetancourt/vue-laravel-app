c99cdab1984a1ec158f5376722f0c0964a78d8e1
Alejandro <aesbetancourt@outlook.com>
2019-08-11 20:24:22 -0400

    Dashboard added a new card

--- resources/js/components/Dashboard.vue
+++ resources/js/components/Dashboard.vue
@@ -1,12 +1,39 @@
 <template>
     <div class="container">
         <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">Dashboard Component</div>
-
-                    <div class="card-body">
-                        I'm an example component.
+            <div class="col-md-10">
+                <div class="row">
+                    <div class="col-lg-7 col-xl-8">
+                        <div class="card shadow mb-4">
+                            <div class="card-header d-flex justify-content-between align-items-center">
+                                <h6 class="text-primary font-weight-bold m-0"><strong>Bienvenido</strong> a la comunidad de libros mas grande de internet</h6>
+                            </div>
+                            <div class="card-body">
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum facere iure nisi obcaecati perspiciatis! Adipisci
+                                    blanditiis delectus earum eius eligendi expedita, fugiat ipsam laudantium,
+                                    libero omnis possimus quasi quia quibusdam sunt tenetur. Eaque ipsa libero maiores molestiae nemo
+                                    possimus, quam. Lorem ipsum dolor sit amet, consectetur adipisicing elit. A dolorum velit vitae!
+                                    Aspernatur delectus dolorum est expedita in iure molestias neque sint vel voluptates! Consectetur
+                                    cumque facere, id illo, magnam magni maiores non quam recusandae, repellendus saepe sit suscipit vero.
+                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci asperiores autem cum error facilis
+                                    illum laudantium officiis placeat quasi sunt?</p>
+                                <button class="btn btn-primary">Lorem.</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-5 col-xl-4">
+                        <div class="card shadow mb-4">
+                            <div class="card-header d-flex justify-content-between align-items-center">
+                                <h6 class="text-primary font-weight-bold m-0">Aquí <strong>encontrarás</strong>...</h6>
+                            </div>
+                            <div class="card-body">
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab, alias autem culpa cum deleniti dolorem
+                                    ea eaque eius eos error esse excepturi facere id incidunt inventore minima,
+                                    nam nemo perspiciatis quisquam ratione repudiandae sed tempora totam vero voluptas voluptatibus voluptatum.
+                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque, incidunt.</p>
+                                <button class="btn btn-primary">Lorem.</button>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
