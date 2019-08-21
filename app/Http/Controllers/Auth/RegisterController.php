d53d4776a354d943f9d7692c73daf69f2896ebdf
Dariana <darianasil1999@hotmail.com>
2019-08-21 11:55:55 -0400

    Language change and name validation

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -49,7 +49,7 @@ class RegisterController extends Controller
     protected function validator(array $data)
     {
         return Validator::make($data, [
-            'name' => ['required', 'string', 'max:255'],
+            'name' => ['required', 'string', 'max:255', 'regex:/^[a-z]+$/i'],
             'phone' => ['required', 'string', 'max:15'],
             'ci' => ['required', 'string', 'max:15'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
