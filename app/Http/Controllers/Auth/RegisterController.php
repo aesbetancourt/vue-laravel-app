6d302d5961b580af8c0d6da53b77f3c06cf2b030
Dariana <darianasil1999@hotmail.com>
2019-09-08 18:10:29 -0400

    Validation phone and cedula

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -51,7 +51,7 @@ class RegisterController extends Controller
         return Validator::make($data, [
             'name' => ['required', 'string', 'max:255', 'regex:/^[a-z]+$/i'],
             'phone' => ['numeric', 'required', 'string', 'max:15'],
-            'ci' => ['numeric','required', 'string',  'min:6','max:12'],
+            'ci' => ['numeric','required', 'string',  'min:6'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
         ]);
